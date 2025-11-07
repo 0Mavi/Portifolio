65386ea381ab7172f1b6ef937ac3bc5353071bb9
Vitoria <mvitoriaberto2@gmail.com>
2025-11-07 11:32:07 -0300

    First Commit

--- src/pages/Home/app.tsx
+++ src/pages/Home/app.tsx
@@ -1,15 +1,8 @@
-import About from "../../components/AboutUs";
-import Hero from "../../components/Hero";
-import InfoSection from "../../components/Info";
-import RobotSection from "../../components/Personagem";
+
 
 export default function Home() {
     return (
         <div>
-           <Hero/>
-           <About/>
-           <InfoSection/>
-           <RobotSection/>
         </div>
     )
 }
